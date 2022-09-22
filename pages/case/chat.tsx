1d570f0b7e9097d1287c76ca4408307da478dbb0
Torrey Searle <tsearle@gmail.com>
2022-09-22 16:16:05 +0200

    prevent wrapped text from overlapping

--- pages/case/chat.tsx
+++ pages/case/chat.tsx
@@ -69,7 +69,7 @@ export const Chat = ({user}: any) => {
                             fontSize: '10px',
                             marginBottom: '10px'
                         }}>{currentCustomer.username}&nbsp;-&nbsp;{msg.channel}</div>
-                        <span style={{background: '#bbb', borderRadius: '3px', padding: '7px 10px'}}>
+                        <span style={{background: '#bbbbbb', lineHeight: '27px', borderRadius: '3px', padding: '7px 10px'}}>
                     <span style={{}}>{msg.message}</span><span style={{marginLeft: '10px'}}>22:13</span>
                     </span>
                     </div>
@@ -78,9 +78,10 @@ export const Chat = ({user}: any) => {
                     <div style={{textAlign: 'right'}}>
                         <div style={{
                             fontSize: '10px',
+                            lineHeight: '16px',
                             marginBottom: '10px'
                         }}>{currentUser.firstName}&nbsp;{currentUser.lastName}</div>
-                        <span style={{background: '#bbb', borderRadius: '3px', padding: '7px 10px'}}>
+                        <span style={{background: '#bbbbbb', lineHeight: '27px', borderRadius: '3px', padding: '7px 10px'}}>
                             <span style={{}}>{msg.message}</span><span style={{marginLeft: '10px'}}>22:13</span>
                         </span>
                     </div>
