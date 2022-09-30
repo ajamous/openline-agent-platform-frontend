25bda3ed5c95d780f50dada7ae64481012d5c467
Eduard Firut <edi@openline.ai>
2022-09-30 16:14:27 +0300

    Secure app + build and publish

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,42 +1,68 @@
 import Header from "./header";
 import LayoutMenu from "./menu";
 import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import {getUserAccount, loadUserAccount, User} from "../../lib/loadUserAccount";
+import {useApi} from "../../lib/useApi";
 
-export default function Layout({ children }: any) {
+export default function Layout({children}: any) {
     const router = useRouter();
+    const [user, setUser] = useState<User | undefined>();
+    const axiosInstance = useApi();
+
+    useEffect(() => {
+        loadUserAccount(axiosInstance).then(() => {
+            if (!getUserAccount()) {
+                router.push('/login'); //todo check if we redirect to login
+            } else {
+                setUser(getUserAccount);
+            }
+        })
+    }, []);
 
     return (
         <>
-            <Header/>
+            {!user &&
+                <div>loading</div>
+            }
 
-            <div className="flex" style={{height: 'calc(100vh - 100px)'}}>
+            {user &&
+                <>
+                    <Header height={'70px'}/>
 
-                {
-                    router.pathname.indexOf("/editor") === -1 &&
-                    <div className="flex-grow-0 flex"
-                         style={{width: '200px', height: '100%'}}>
+                    <div className="flex" style={{height: 'calc(100vh - 90px)'}}>
 
-                        <div style={{width: '100%', height: '100%', padding: '10px 0px 10px 10px', overflow: 'hidden'}}>
+                        <div className="flex-grow-0 flex"
+                             style={{width: '200px', height: '100%'}}>
 
-                            <LayoutMenu/>
+                            <div style={{
+                                width: '100%',
+                                height: '100%',
+                                padding: '10px 0px 10px 10px',
+                                overflow: 'hidden'
+                            }}>
+
+                                <LayoutMenu/>
+
+                            </div>
 
                         </div>
 
+                        <div className="flex-grow-1 flex" style={{height: '100%'}}>
+                            <div style={{
+                                width: '100%',
+                                height: '100%',
+                                margin: '10px',
+                                border: '1px solid #0b213f',
+                                borderRadius: '6px'
+                            }}>
+                                {children}
+                            </div>
+                        </div>
                     </div>
-                }
+                </>
+            }
 
-                <div className="flex-grow-1 flex" style={{height: '100%'}}>
-                    <div style={{
-                        width: '100%',
-                        height: '100%',
-                        margin: '10px',
-                        border: '1px solid #0b213f',
-                        borderRadius: '6px'
-                    }}>
-                        {children}
-                    </div>
-                </div>
-            </div>
         </>
     )
 }
--- pages/case/index.tsx
+++ pages/case/index.tsx
@@ -3,22 +3,25 @@ import {DataTable} from 'primereact/datatable';
 import {useEffect, useState} from "react";
 import {Column} from "primereact/column";
 import axios from "axios";
-import SockJsClient from 'react-stomp';
 import {Button} from "primereact/button";
 import {useRouter} from "next/router";
 import {Toolbar} from "primereact/toolbar";
 import {Fragment} from "preact";
-
+import Layout from "../../components/layout/layout";
 
 
 const Index: NextPage = () => {
     const router = useRouter();
-    const [flows, setFlows] = useState([] as any);
+    const SockJsClient = require('react-stomp');
+
+    const [cases, setCases] = useState([] as any);
+    const [initializeWebsocket, setInitializeWebsocket] = useState(false);
 
     useEffect(() => {
         axios.get(`${process.env.NEXT_PUBLIC_BE_PATH}/case`)
             .then(res => {
-                setFlows(res.data.content);
+                setCases(res.data.content);
+                setInitializeWebsocket(true);
             })
     }, []);
 
@@ -36,27 +39,32 @@ const Index: NextPage = () => {
         console.log("Got a new case!");
         axios.get(`${process.env.NEXT_PUBLIC_BE_PATH}/case`)
             .then(res => {
-                setFlows(res.data.content);
+                setCases(res.data.content);
             });
     }
 
     return (
         <>
-            <div>
-            <SockJsClient
-                url={`${process.env.NEXT_PUBLIC_WEBSOCKET_PATH}/websocket`}
-                topics={[`/queue/cases`]}
-                onConnect={console.log("Connected!")}
-                onDisconnect={console.log("Disconnected!")}
-                onMessage={(msg: any) => handleWebsocketMessage(msg)}
-                debug={true}
-            /></div>
-            <Toolbar left={leftContents}/>
-            <DataTable value={flows}>
-                <Column field="userName" header="Name"></Column>
-                <Column field="state" header="State"></Column>
-                <Column field="actions" header="Actions" align={'right'} body={actionsColumn}></Column>
-            </DataTable>
+            <Layout>
+                {
+                    initializeWebsocket &&
+                    <SockJsClient
+                        url={`${process.env.NEXT_PUBLIC_WEBSOCKET_PATH}/websocket`}
+                        topics={[`/queue/cases`]}
+                        onConnect={console.log("Connected!")}
+                        onDisconnect={console.log("Disconnected!")}
+                        onMessage={(msg: any) => handleWebsocketMessage(msg)}
+                        debug={true}
+                    />
+                }
+
+                <Toolbar left={leftContents}/>
+                <DataTable value={cases}>
+                    <Column field="userName" header="Name"></Column>
+                    <Column field="state" header="State"></Column>
+                    <Column field="actions" header="Actions" align={'right'} body={actionsColumn}></Column>
+                </DataTable>
+            </Layout>
         </>
     );
 }
